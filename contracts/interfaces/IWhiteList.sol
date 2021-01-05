585faeb4adfde0812416d30d807cc58f13c872d4
HappyFeet07 <jack0978239920@gmail.com>
2021-01-05 22:00:31 +0800

    Change WhiteList to Whitelist, remove ViewAllWhitelist and add check function work as if the address is on whitelist.

--- contracts/interfaces/IWhiteList.sol
+++ contracts/interfaces/IWhiteList.sol
@@ -1,9 +1,8 @@
 pragma solidity ^0.8.0;
 
-interface WhiteList {
+interface Whitelist {
     
     // Enroll, BatchEnroll, Remove, BatchRemove are emit when actions are called.
-    // ViewList is emit when viewWhiteList are called.
     // Two Types of _type are suppliers and anchors.
     
     event Enroll(address indexed _address, string _type);
@@ -14,16 +13,16 @@ interface WhiteList {
     
     event BatchRemove(address[] indexed _addresses, string _type);
     
-    event ViewList(string _type);
+    function enrollWhitelist(address _address, string calldata _type) external returns(bool);
     
-    function enrollWhiteList(address _address, string calldata _type) external returns(bool);
+    function enrollWhitelist(address[] calldata _addresses, string calldata _type) external returns(bool);
     
-    function enrollWhiteList(address[] calldata _addresses, string calldata _type) external returns(bool);
+    function removeWhitelist(address _address, string calldata _type) external returns(bool);
     
-    function removeWhiteList(address _address, string calldata _type) external returns(bool);
+    function removeWhitelist(address[] calldata _addresses, string calldata _type) external returns(bool);
     
-    function removeWhiteList(address[] calldata _addresses, string calldata _type) external returns(bool);
-    
-    function viewWhiteList() external view returns (address[] memory);
+    function inWhitelist(address _address, string calldata _type) external view returns (bool);
+
+	function batchInWhitelist(address[] calldata _addresses, string calldata _type) external view returns (bool);
 }
 
