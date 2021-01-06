15a9c1436f57c592dd6829ccc60b58cbdfc38acf
StillFantastic <bl33234679@gmail.com>
2021-01-06 22:46:57 +0800

    Add token factory interface

---
+++ contracts/interfaces/ITokenFactory.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+interface ITokenFactory {
+    // set _needTime to true if want to know holding time, set _needCopy to true if need a recording token
+    function createToken(uint256 _supply, address _receiver, bool _needTime, bool _needCopy) external returns(uint256);
+    function setTimeInterval(uint256 _startTime, uint256 _endTime) external;
+}
