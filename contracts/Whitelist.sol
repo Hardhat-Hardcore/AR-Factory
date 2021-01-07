572ce7ffec3a7f6a81d9ab4f3ed0ac6c44b6f0cf
HappyFeet07 <jack0978239920@gmail.com>
2021-01-07 19:17:25 +0800

    Whitelist interface modified, add untested whitelist, add Address.sol into utils and install ethersjs on hardhat

---
+++ contracts/Whitelist.sol
@@ -0,0 +1,60 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.0;
+
+import './interfaces/IWhitelist.sol';
+import './libraries/AccessControl.sol';
+
+contract Whitelist is IWhitelist, AccessControl {
+    
+    bytes32 public constant SUPPLIER_ROLE = keccak256("SUPPLIER_ROLE");
+
+    bytes32 public constant ANCHOR_ROLE = keccak256("ANCHOR_ROLE");
+    
+    constructor(address _root) {
+        _setupRole(DEFAULT_ADMIN_ROLE, _root);
+        _setRoleAdmin(SUPPLIER_ROLE, DEFAULT_ADMIN_ROLE);
+        _setRoleAdmin(ANCHOR_ROLE, DEFAULT_ADMIN_ROLE);
+    }
+    
+    modifier onlyAdmin() {
+        require(isAdmin(_msgSender()), "Restricted to admins.");
+        _;
+    }
+
+    function isAdmin(address _account) public view returns (bool) {
+        return hasRole(DEFAULT_ADMIN_ROLE, _account);
+    }
+    
+    function inSupplier(address _account) external override view returns (bool) {
+        return hasRole(SUPPLIER_ROLE, _account);
+    }
+    
+    function inAnchor(address _address) external override view returns (bool) {
+        return hasRole(ANCHOR_ROLE, _address);
+    }
+
+    function addAdmin(address _account) public onlyAdmin {
+        grantRole(DEFAULT_ADMIN_ROLE, _account);
+    }
+    
+    function renounceAdmin() public {
+        renounceRole(DEFAULT_ADMIN_ROLE, _msgSender());
+    }
+    
+    function addSupplier(address _account) public onlyAdmin {
+        grantRole(SUPPLIER_ROLE, _account);
+    }
+    
+    function addAnchor(address _account) public onlyAdmin {
+        grantRole(ANCHOR_ROLE, _account);
+    }
+    
+    function removeSupplier(address _account) public onlyAdmin {
+        revokeRole(SUPPLIER_ROLE, _account);
+    }
+    
+    function removeAnchor(address _account) public onlyAdmin {
+        revokeRole(ANCHOR_ROLE, _account);
+    }
+}
