13595ba01dd7e6121b1ec591e2760ec2f618391d
StillFantastic <bl33234679@gmail.com>
2021-01-09 16:02:58 +0800

    Remove comma

--- contracts/interfaces/ITokenFactory.sol
+++ contracts/interfaces/ITokenFactory.sol
@@ -4,6 +4,6 @@ pragma solidity ^0.8.0;
 interface ITokenFactory {
     // set _needTime to true if want to know holding time, set _needCopy to true if need a recording token
     // return token id
-    function createToken(uint256 _supply, address _receiver, , address _operator, bool _needTime, bool _needCopy) external returns(uint256);
+    function createToken(uint256 _supply, address _receiver, address _operator, bool _needTime, bool _needCopy) external returns(uint256);
     function setTimeInterval(uint256 _startTime, uint256 _endTime) external;
 }
