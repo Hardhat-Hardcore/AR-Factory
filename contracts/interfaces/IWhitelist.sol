69e4f4d2be5a4840d08866be764e5a3382e8e382
StillFantastic <bl33234679@gmail.com>
2021-01-09 15:30:39 +0800

    Widen pragma

--- contracts/interfaces/IWhitelist.sol
+++ contracts/interfaces/IWhitelist.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity 0.8.0;
+pragma solidity ^0.8.0;
 
 interface IWhitelist { 
     function inWhitelist(address _address) external view returns (bool);
