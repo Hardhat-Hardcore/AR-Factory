15faca8020c10bf611f47f31a9598e7382d66f1e
StillFantastic <bl33234679@gmail.com>
2021-01-09 18:47:01 +0800

    make create token polymorphism

--- contracts/interfaces/ITokenFactory.sol
+++ contracts/interfaces/ITokenFactory.sol
@@ -5,5 +5,6 @@ interface ITokenFactory {
     // set _needTime to true if want to know holding time, set _needCopy to true if need a recording token
     // return token id
     function createToken(uint256 _supply, address _receiver, address _operator, bool _needTime, bool _needCopy) external returns(uint256);
+    function createToken(uint256 _supply, address _receiver, address _operator, bool _needTime, bool _needCopy, string calldata _uri) external returns(uint256);
     function setTimeInterval(uint256 _startTime, uint256 _endTime) external;
 }
