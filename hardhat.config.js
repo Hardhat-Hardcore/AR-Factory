369b4cc86d885db7bd8535370ec20025f3532cf8
boyuchen0224 <boyuchen0224@gmail.com>
2021-01-30 00:51:13 +0800

    Add hardhat network

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,12 +1,14 @@
 require("@nomiclabs/hardhat-waffle")
 require("@nomiclabs/hardhat-web3");
-require('hardhat-contract-sizer')
-require('hardhat-spdx-license-identifier')
+require("hardhat-contract-sizer")
+require("hardhat-spdx-license-identifier")
 require("solidity-coverage")
 
 /**
- * @type import('hardhat/config').HardhatUserConfig
+ * @type import("hardhat/config").HardhatUserConfig
  */
+
+const mnemonic = process.env.MNEMONIC || "test test test test test test test test test test test junk"
 
 module.exports = {
   networks: {
@@ -14,9 +16,18 @@ module.exports = {
       url: "http://127.0.0.1:8545",
       accounts: [
         "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80",
-        "0x9c50ad240ce6754cd811acc24ee64e00055ec8c00ce33a2f7106ac27056c74eb",
-        "0x1b836492a0524013e039b94184be9fead819053e32979d9d672e534b6c9740b9"
+        "0x59c6995e998f97a5a0044966f0945389dc9e86dae88c7a8412f4603b6b78690d",
+        "0x5de4111afa1a4b94908f83103eb1f1706367c2e68ca870fc3fb9a804cdab365a"
       ],
+    },
+    hardhat: {
+      accounts: {
+        mnemonic,
+        path: "m/44'/60'/0'/0",
+        count: 5,
+        gasPrice: 0,
+        gasLimit: 0x1fffffffffffff,
+      }
     },
     forking: {
       url: "https://eth-mainnet.alchemyapi.io/v2/wOanzc8-3oDY4oNTJxmbwuijaJ6QILFH",
