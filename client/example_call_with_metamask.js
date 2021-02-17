2af4760985cf9804e90873ed40380adf5d08af55
boyuchen0224 <boyuchen0224@gmail.com>
2021-02-17 13:29:29 +0800

    Change to ropsten

--- client/example_call_with_metamask.js
+++ client/example_call_with_metamask.js
@@ -1,8 +1,8 @@
 const gsn = require('@opengsn/gsn')
 const ethers = require('ethers')
 
-const paymasterAddr = '0x99EFc0936FC840462794c03Dbb13698aEE58f205'
-const contractAddr = '0x08D1801e984F508Af643db8e069FddEF197400bc'
+const paymasterAddr = '0x14912F704b4Eaa1Eb62fd76eD72FA9A0B61d9C16'
+const contractAddr = '0x9aDa76938FaA7483520EB5237E08547C7579bc9e'
 const { abi: contractAbi } = require('../artifacts/contracts/mock/TokenFactoryMock.sol/TokenFactoryMock.json')
 
 let provider
